37bb7e5534bc175c38b22482f7caead6c6ae7bcd
Noah Roberts <noahmroberts@gmail.com>
2021-07-12 19:15:57 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const errorHandler = require('./error-handler')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(helmet())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(errorHandler)
+
+module.exports = app
