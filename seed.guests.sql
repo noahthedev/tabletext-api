eba2ca0415197566b0b996cb8f78c6d93a22cc9a
Noah Roberts <noahmroberts@gmail.com>
2021-08-01 14:33:44 -0500

    seeded

---
+++ seed.guests.sql
@@ -0,0 +1,8 @@
+INSERT INTO waitlist (guestname, guestcount, phone)
+VALUES
+  ('Barack Obama', 4, '+11234567890'),
+  ('Noah Roberts', 2, '+11234567890'),
+  ('Joe Rogan', 8, '+11234567890'),
+  ('Tom Waits', 6, '+11234567890'),
+  ('John Galt', 2, '+11234567890'),
+  ('Holden Caulfield', 1, '+11234567890');
