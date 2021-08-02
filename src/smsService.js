cec83a65650981018a27c07d10034e07c91aab54
Noah Roberts <noahmroberts@gmail.com>
2021-08-02 14:22:02 -0500

    sms working

--- src/smsService.js
+++ src/smsService.js
@@ -2,17 +2,20 @@ const express = require('express')
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 
 const smsService = express()
+const jsonParser = express.json()
 
 smsService
-  .get('/', (req, res) => {
+  .route('/')
+  .post(jsonParser, (req, res) => {
+    const { phone } = req.body;
     client.messages
       .create({
         body: 'Your table is ready',
         from: '+12406182097',
-        to: '+18152577009'
+        to: `${phone}`
       })
-      .then(res.send('SMS sent'))
-  })
+      .then(res.send(`SMS sent to ${phone}`))
+  }) 
 
 module.exports = smsService
 
