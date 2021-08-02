5649a3834bbe2d0dbc40e14472ca60518e47bbe4
Noah Roberts <noahmroberts@gmail.com>
2021-08-02 09:59:29 -0500

    added router methods and services

--- src/waitlist/waitlist-router.js
+++ src/waitlist/waitlist-router.js
@@ -23,6 +23,54 @@ waitlistRouter
         res.json(guests.map(serializeGuest))
       })
   })
+  .post(jsonParser, (req, res, next) => {
+    const { guestname, guestcount, phone } = req.body
+
+    const newGuest = { guestname, guestcount, phone }
+
+    WaitlistService.insertGuest(
+      req.app.get('db'), newGuest
+    )
+      .then(guest => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `${guest.id}`))
+          .json(serializeGuest(guest))
+      })
+      .catch(next)
+  })
+
+  waitlistRouter
+    .route('/:id')
+    .all((req, res, next) => {
+      const knexInstance = req.app.get('db')
+      WaitlistService.getById(
+        knexInstance,
+        req.params.id
+      )
+        .then(guest => {
+          if (!guest) {
+            return res.status(404).json({
+              error: { message: `Guest doesn't exist` }
+            })
+          }
+          res.guest = guest
+          .next()
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+      res.json(serializeGuest(res.guest))
+    })
+    .delete((req, res, next) => {
+      const knexInstance = req.app.get('db')
+      WaitlistService.deleteGuest(
+        knexInstance,
+        req.params.id
+      )
+      .then(res.status(204).end())
+      .catch(next)
+    })
 
 module.exports = waitlistRouter  
 
--- src/waitlist/waitlist-service.js
+++ src/waitlist/waitlist-service.js
@@ -3,7 +3,34 @@ const WaitlistService = {
     return knex
       .select('*')
       .from('waitlist')
+  },
+  getById(knex, id) {
+    return knex
+      .from('waitlist')
+      .select('*')
+      .where('id', id)
+      .first()
+  },
+  insertGuest(knex, newGuest) {
+    return knex
+      .insert(newGuest)
+      .into('waitlist')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  deleteGuest(knex, id) {
+    return knex('waitlist')
+      .where({ id })
+      .delete()
+  },
+  updateRecipe(knex, id, guestMods) {
+    return knex('waitlist')
+      .where({ id })
+      .update(guestMods)
   }
+
 }
 
 module.exports = WaitlistService
